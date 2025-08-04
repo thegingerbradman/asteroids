b85672f0ba91e4ad499ef415578c9bf6c44b659f
Brade <bradfordhege@gmail.com>
2025-08-03 20:22:02 -0400

    Added player movement functionality

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pygame
-from constants import (SCREEN_WIDTH, SCREEN_HEIGHT,
-                       ASTEROID_MIN_RADIUS,
-                       ASTEROID_KINDS,
-                       ASTEROID_SPAWN_RATE,
-                       ASTEROID_MAX_RADIUS)
+from constants import(SCREEN_WIDTH, SCREEN_HEIGHT,
+                      ASTEROID_MIN_RADIUS,
+                      ASTEROID_KINDS,
+                      ASTEROID_SPAWN_RATE,
+                      ASTEROID_MAX_RADIUS)
 from player import Player
 def main():
     print("Starting Asteroids!")
