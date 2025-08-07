39df2edbad9cb7892d6c065ebd92e9e0680dd671
Brade <bradfordhege@gmail.com>
2025-08-06 22:10:20 -0400

    added asteroid/bullet collision and kill method

--- main.py
+++ main.py
@@ -45,6 +45,10 @@ def main():
             if asteroid.collision(player):
                 print("Game over!")
                 return
+            for shot in shots:
+                if asteroid.collision(shot):
+                    asteroid.kill()
+                    shot.kill()
         pygame.display.flip()
 
 if __name__ == "__main__":
